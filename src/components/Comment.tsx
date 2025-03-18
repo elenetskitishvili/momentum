5d6d6ca18448a6edd248771f122c6d2242995b13
elenetskitishvili <elene.tskitishvili504@gmail.com>
2025-03-18 20:00:07 +0400

    feat: display replies on the ui

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -53,24 +53,28 @@ export default function Comment({
         />
       </div>
 
-      {/* Replies */}
-      {/* <div className="ml-10 flex items-start gap-3 mb-2.5">
-        <Image
-          src={"/icons/user-icon.svg"}
-          alt=""
-          width={38}
-          height={38}
-          className="object-cover rounded-full"
-        />
-        <div>
-          <p className="text-lg font-medium text-primary-text mb-2">
-            ნატალია გიორგაძე
-          </p>
-          <p className="text-light-text font-[350] text-base leading-[100%] mb-2.5">
-            დიზაინი სუფთად ჩანს, მაგრამ კოდირებისას მნიშვნელოვანი იქნება.
-          </p>
+      {[...(comment.sub_comments ?? [])].reverse().map((subComment) => (
+        <div
+          key={subComment.id}
+          className="ml-10 flex items-start gap-3 mb-2.5"
+        >
+          <Image
+            src={subComment.author_avatar}
+            alt={subComment.author_nickname}
+            width={38}
+            height={38}
+            className="object-cover rounded-full"
+          />
+          <div>
+            <p className="text-lg font-medium text-primary-text mb-2">
+              {subComment.author_nickname}
+            </p>
+            <p className="text-light-text font-[350] text-base leading-[100%] mb-2.5">
+              {subComment.text}
+            </p>
+          </div>
         </div>
-      </div> */}
+      ))}
     </div>
   );
 }
